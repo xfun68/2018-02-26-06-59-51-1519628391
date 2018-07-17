7a0e73f7455f9ab097dcee418e6707cb57946e2d
Xiaofeng Wang <xfun68@gmail.com>
2018-07-18 06:36:11 +0800

    finish practice 4

--- src/practice_4/worker.js
+++ src/practice_4/worker.js
@@ -1,1 +1,17 @@
+"use strict";
 
+import Person from "../../src/practice_1/person.js";
+
+class Worker extends Person {
+
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Worker. I have a job.`;
+    }
+
+}
+
+export default Worker;
