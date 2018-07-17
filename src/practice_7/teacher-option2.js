9bd50ef757319850f44e71e5bd99bf23de326557
Xiaofeng Wang <xfun68@gmail.com>
2018-07-18 07:04:57 +0800

    finish practice 7 optional part (WTF: just passed after pasting the code)

--- src/practice_7/teacher-option2.js
+++ src/practice_7/teacher-option2.js
@@ -1,1 +1,24 @@
+"use strict";
 
+import Person from "../../src/practice_5/Person.js";
+
+class Teacher extends Person {
+
+    constructor(name, age, klass) {
+        super(name, age)
+        this.klass = klass;
+    }
+
+    introduce() {
+        let taughtClass = this.klass === undefined ? "No Class" : `${this.klass.getDisplayName()}`;
+        return `${super.introduce()} I am a Teacher. I teach ${taughtClass}.`;
+    }
+
+    introduceWith(student) {
+        let teachOrNot = student.klass === this.klass ? "teach" : "don't teach";
+        return `${super.introduce()} I am a Teacher. I ${teachOrNot} ${student.name}.`;
+    }
+
+}
+
+export default Teacher;
