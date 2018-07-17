77d99a55a8d29bfa74244678a47ce38b502fbb4f
Xiaofeng Wang <xfun68@gmail.com>
2018-07-18 07:02:50 +0800

    finish practice 7 basic part

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,15 @@
+"use strict";
 
+class Class {
+
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+}
+
+export default Class;
--- src/practice_7/person.js
+++ src/practice_7/person.js
@@ -1,1 +1,5 @@
+"use strict";
 
+import Person from "../../src/practice_1/person.js";
+
+export default Person;
