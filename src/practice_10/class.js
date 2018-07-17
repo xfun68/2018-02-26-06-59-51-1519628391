9f7e0f234dac9575328ace8392a84bf9093e93b3
Xiaofeng Wang <xfun68@gmail.com>
2018-07-18 07:46:28 +0800

    finish practice 10

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,26 @@
+"use strict";
 
+class Class {
+
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student) {
+        if (this.number != student.klass.number) {
+            console.log("It is not one of us.");
+            return;
+        }
+        this.leader = student;
+    }
+
+    appendMember(student) {
+        student.klass = this;
+    }
+}
+
+export default Class;
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,33 @@
+"use strict";
 
+import _ from "lodash";
+
+import Person from "../../src/practice_8/Person.js";
+
+class Teacher extends Person {
+
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Teacher. I teach ${this.taughtClasses()}.`;
+    }
+
+    taughtClasses() {
+        if (this.hasNoClasses()) {
+            return "No Class";
+        }
+
+        let classNumberList = this.klasses.map(klass => klass.number).join(', ');
+        return `Class ${classNumberList}`;
+    }
+
+    hasNoClasses() {
+        return this.klasses === undefined || _.isEmpty(this.klasses);
+    }
+
+}
+
+export default Teacher;
