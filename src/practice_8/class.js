90cb8983ed3c0f456f4d6269a75777ee77631496
Xiaofeng Wang <xfun68@gmail.com>
2018-07-18 07:29:30 +0800

    finish practice 8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,22 @@
+"use strict";
 
+class Class {
+
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student) {
+        if (this.number != student.klass.number) {
+            return;
+        }
+        this.leader = student;
+    }
+
+}
+
+export default Class;
--- src/practice_8/teacher.js
+++ src/practice_8/teacher.js
@@ -1,1 +1,19 @@
+"use strict";
 
+import Person from "../../src/practice_8/Person.js";
+
+class Teacher extends Person {
+
+    constructor(id, name, age, klass) {
+        super(id, name, age)
+        this.klass = klass;
+    }
+
+    introduce() {
+        let taughtClass = this.klass === undefined ? "No Class" : `${this.klass.getDisplayName()}`;
+        return `${super.introduce()} I am a Teacher. I teach ${taughtClass}.`;
+    }
+
+}
+
+export default Teacher;
