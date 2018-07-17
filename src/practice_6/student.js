3071a9961fdb4e2012eec049c0234d4cbedc22ee
Xiaofeng Wang <xfun68@gmail.com>
2018-07-18 06:52:31 +0800

    finish practice 6

--- src/practice_6/student.js
+++ src/practice_6/student.js
@@ -1,1 +1,5 @@
+"use strict";
 
+import Student from "../../src/practice_5/student.js";
+
+export default Student;
--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,19 @@
+"use strict";
 
+import Person from "../../src/practice_5/Person.js";
+
+class Teacher extends Person {
+
+    constructor(name, age, klass) {
+        super(name, age)
+        this.klass = klass;
+    }
+
+    introduce() {
+        let taughtClass = this.klass === undefined ? "No Class" : `Class ${this.klass}`;
+        return `${super.introduce()} I am a Teacher. I teach ${taughtClass}.`;
+    }
+
+}
+
+export default Teacher;
